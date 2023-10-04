4e1613ada6bfee5bce9318cdc71de630523100bd
dailesprojetos <dailesprojetos.softwares@gmail.com>
2023-10-04 11:52:14 -0300

    Atualização de Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent},
+  { path: '', component:HomeComponent}
 ];
 
 @NgModule({
