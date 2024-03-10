6353fb3c9c4243f85824ade5e94f823b66ee9c4e
dailesprojetos <dailesprojetos.softwares@gmail.com>
2024-03-10 01:59:47 -0300

    Atualização Conexao

--- src/app/condominios.service.ts
+++ src/app/condominios.service.ts
@@ -12,22 +12,22 @@ export class CondominiosService {
   constructor(private http: HttpClient) {}
 
   salvar(condominio: Condominio) : Observable<Condominio>{
-    return this.http.post<Condominio>(Conexao.baseURL+'/condwebapi/condominios',condominio);
+    return this.http.post<Condominio>(Conexao.baseURL+'/condwebapi/condwebapi/condominios',condominio);
   }
 
   atualizar(condominio: Condominio) : Observable<any>{
-    return this.http.put<Condominio>(Conexao.baseURL+`/condwebapi/condominios/cnpj/${condominio.cnpj}`,condominio);
+    return this.http.put<Condominio>(Conexao.baseURL+`/condwebapi/condwebapi/condominios/cnpj/${condominio.cnpj}`,condominio);
   }
 
   deletar(condominio: Condominio) : Observable<any>{
-    return this.http.delete<any>(`http://localhost:8080/condwebapi/condominios/${condominio.codigo}`);
+    return this.http.delete<any>(`http://localhost:8080/condwebapi/condwebapi/condominios/${condominio.codigo}`);
   }
 
   getCondominios(): Observable<Condominio[]>{
-    return this.http.get<Condominio[]>(Conexao.baseURL+'/condwebapi/condominios')
+    return this.http.get<Condominio[]>(Conexao.baseURL+'/condwebapi/condwebapi/condominios')
   }
 
   getCondominioByCnpj(cnpj: string) : Observable<Condominio>{
-    return this.http.get<any>(`http://localhost:8080/condwebapi/condominios/cnpj/${cnpj}`);
+    return this.http.get<any>(`http://localhost:8080/condwebapi/condwebapi/condominios/cnpj/${cnpj}`);
   }
 }
